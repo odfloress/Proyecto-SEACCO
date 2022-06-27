2481a035d7ec2a86662a293cc671fd6dd9a98244
odfloress <odfloress@unah.hn>
2022-06-26 23:51:39 -0600

    cambios 26 de junio

--- controladores/co_login.php
+++ controladores/co_login.php
@@ -9,6 +9,7 @@
 
   // Desencripta la contraseña
   $contrasena= hash('sha512', $contrasena);
+ 
 
   // Inicio del switch, para validar el valor del boton accion
   switch($accion){
@@ -19,51 +20,76 @@
      $result = mysqli_query($conn, $validar_usuario); 
       if (mysqli_num_rows($result) > 0) { 
 
-          // Valida los intentos fallidos en la tabla tbl_usuarios 
-          $validar_intento = "SELECT * FROM tbl_usuarios WHERE usuario='$usuario' and intentos<4";
-          $result1 = mysqli_query($conn, $validar_intento);
-            if (mysqli_num_rows($result1) > 0) {
+           // Valida la configuración de intentos fallidos de la tabla tbl_parametros
+            $validar_conf_intentos = "SELECT * FROM tbl_parametros WHERE PARAMETRO='INTENTOS'";
+            $result2 = mysqli_query($conn, $validar_conf_intentos);
+            if (mysqli_num_rows($result2) > 0) {
+                
+                while($row = mysqli_fetch_assoc($result2)) {
+                      $intento =  $row["VALOR"];
 
-                // Valida cantidad de preguntas en la tabla tbl_usuarios 
-                $validar_pregunta = "SELECT * FROM tbl_usuarios WHERE usuario='$usuario' and CANT_PREGUNTAS>7 ";
-                $result2 = mysqli_query($conn, $validar_pregunta);
-                  if (mysqli_num_rows($result2) > 0) {
+                  // Valida los intentos fallidos en la tabla tbl_usuarios 
+                   $validar_intento = "SELECT * FROM tbl_usuarios WHERE usuario='$usuario' and intentos<$intento";
+                   $result3 = mysqli_query($conn, $validar_intento);
+                  } //fin del primer while 
+                   if (mysqli_num_rows($result3) > 0) {
+                        
 
-                      $_SESSION['usuario'] = $usuario;
-                     header('Location: vistas/tablero/vista_tablero.php');
+                        // Valida la configuración de preguntas de la tabla tbl_parametros
+                        $validar_conf_preguntas = "SELECT * FROM tbl_parametros WHERE PARAMETRO='PREGUNTAS'";
+                        $result4 = mysqli_query($conn, $validar_conf_preguntas);
+                        if (mysqli_num_rows($result4) > 0) {
+                          
+                            while($row = mysqli_fetch_assoc($result4)) {
+                              $pregunta =  $row["VALOR"];
 
-                      //Deja en cero los intentos fallidos
-                       $conn = new mysqli($servername, $username, $password, $dbname);
-                       $sql =  "UPDATE tbl_usuarios SET intentos=0 WHERE usuario='$usuario'";
-                       if ($conn->query($sql) === TRUE) {}
-                       exit() ;
+                              // Valida las preguntas en la tabla tbl_usuarios 
+                              $validar_pregunta = "SELECT * FROM tbl_usuarios WHERE usuario='$usuario' and CANT_PREGUNTAS<$pregunta";
+                              $result5 = mysqli_query($conn, $validar_pregunta);
+                            }//fin del segundo while
+                            if (mysqli_num_rows($result5) > 0) {
+                                header('Location: http://localhost/SEACCO/vistas/iniciar_sesion/preguntas_seguridad');
+                              
+                            }else {// Fin del if que valida las preguntas de la tabla tbl_usuarios e inicia el else
+                                     $_SESSION['usuario'] = $usuario;
+                                     header('Location: vistas/tablero/vista_tablero.php');
+                                     
+                                     //Deja en cero los intentos fallidos
+                                      $conn = new mysqli($servername, $username, $password, $dbname);
+                                      $sql =  "UPDATE tbl_usuarios SET intentos=0 WHERE usuario='$usuario'";
+                                      if ($conn->query($sql) === TRUE) {}
+                                      exit() ;
+                                  } // fin del else de validar preguntas
 
-                  }else{ //redirecciona a la vista preguntas_seguridad
+
+                        }else{echo "no existe la configuracion";} // Fin del if que valida si existe el parametro Preguntas e inicia el else
+
+                   }else{// Fin del if que valida los intentos fallidos de la tabla tbl_usuarios e inicia el else
+                          echo '<script>
+                                  alert("Bloqueado por intentos fallidos");
+                                  window.Location = "/_login.php";
+                                </script>';
+                          mysqli_close($conn);
+                        }
+     
                   
-                          header('Location: http://localhost/SEACCO/vistas/iniciar_sesion/preguntas_seguridad');
-                       }
 
-            }else{
-                  // Da una alerta si supero los intentos fallidos y cierra cualquier conexion a la BD
-                  echo '<script>
-                          alert("Bloqueado por intentos fallidos");
-                          window.Location = "/_login.php";
-                        </script>';
-                        mysqli_close($conn);       
-                  }
-               
-     }else{
-          // Suma los intentos fallidos en la tabla tbl_usuarios y cierra cualquier conexion a la BD
-           $conn = new mysqli($servername, $username, $password, $dbname);
-           $sql =  "UPDATE tbl_usuarios SET intentos=intentos+1 WHERE usuario='$usuario'";
-            if ($conn->query($sql) === TRUE) {
-             echo '<script>
-                      alert("Usuario o contraseña invalidos");
-                      window.Location = "/_login.php";
-                   </script>';
-                   mysqli_close($conn);
-             } 
-          }                   
+            }else{echo "no existe la configuracion";} // Fin del if que valida si existe el parametro intentos e inicia el else
+
+          
+      }else{ // fin del if principal e inicio del else
+
+            // Suma los intentos fallidos en la tabla tbl_usuarios y cierra cualquier conexion a la BD
+             $conn = new mysqli($servername, $username, $password, $dbname);
+             $sql =  "UPDATE tbl_usuarios SET intentos=intentos+1 WHERE usuario='$usuario'";
+              if ($conn->query($sql) === TRUE) {
+               echo '<script>
+                        alert("Usuario o contraseña invalidos");
+                        window.Location = "/_login.php";
+                     </script>';
+                     mysqli_close($conn);
+               } 
+            }                    
     break;
       // si el valor del boton no es ingresar cierra cualquier conexion que exista a la BD
     default:
--- controladores/con_preguntas.php
+++ controladores/con_preguntas.php
@@ -1,51 +1,48 @@
 <?php
-  require '../conexion/conexion.php';
+  // require '../conexion/conexion.php';
 
-  //Variables para recuperar la información de los campos de la vista preguntas_seguridad
-  $pregunta=(isset($_POST['preguntas']))?$_POST['preguntas']:"";
-  $respuesta=(isset($_POST['respuesta']))?$_POST['respuesta']:"";
+  // //Variables para recuperar la información de los campos de la vista preguntas_seguridad
+  // $pregunta=(isset($_POST['preguntas']))?$_POST['preguntas']:"";
+  // $respuesta=(isset($_POST['respuesta']))?$_POST['respuesta']:"";
    
-  $accion=(isset($_POST['accion']))?$_POST['accion']:"";
+  // $accion=(isset($_POST['accion']))?$_POST['accion']:"";
 
-  switch($accion){
-      case "guardar": 
+  // switch($accion){
+  //     case "guardar": 
 
-        // validar que exista la pregunta en la tabla tbl_preguntas
-           $validar_pregunta = "SELECT * FROM tbl_preguntas WHERE PREGUNTA='$pregunta'";
-           $result = mysqli_query($conn, $validar_pregunta);
-           if (mysqli_num_rows($result) > 0) {
+  //       // validar que exista la pregunta en la tabla tbl_preguntas
+  //          $validar_pregunta = "SELECT * FROM tbl_preguntas WHERE PREGUNTA='$pregunta'";
+  //          $result = mysqli_query($conn, $validar_pregunta);
+  //          if (mysqli_num_rows($result) > 0) {
                 
-                // Inserta la respuesta en la tabla tbl_respuestas_usuario
-                $sql = "INSERT INTO tbl_respuestas_usuario (ID_PREGUNTA, RESPUESTA)
-                        VALUES ('2', '$respuesta')";
-                       
-                       if (mysqli_query($conn, $sql)) {
-                            echo '<script>
-                                    alert("Respuesta guardada con exito");
-                                    window.Location = "/login.php";
-                                  </script>';
+            
+  //             while($row = mysqli_fetch_assoc($result)) {
+  //               echo  $row["PREGUNTA"];
+              
+  //             }
+           
 
-                                // Suma las preguntas en la tabla tbl_usuarios 
-                                 $conn = new mysqli($servername, $username, $password, $dbname);
-                                 $sql =  "UPDATE tbl_usuarios SET CANT_PREGUNTAS=CANT_PREGUNTAS+1";
-                                 if ($conn->query($sql) === TRUE) {}   
-                       }else{
-                                  echo '<script>
-                                            alert("Error al intentar guardar la respuesta");
-                                            window.Location = "/login.php";
-                                        </script>';                                  
-                                         mysqli_error($conn);
-                            } 
+
+
+
+
+  //                      }else{
+  //                                 echo '<script>
+  //                                           alert("Error al intentar guardar la respuesta");
+  //                                           window.Location = "/login.php";
+  //                                       </script>';                                  
+  //                                        mysqli_error($conn);
+                           
                
     
-            } // fin del else principal
+  //           } // fin del else principal
              
-      break;
+  //     break;
       
-        default:
+  //       default:
           
-          $conn->close();   
-  }// Fin del switch, para validar el valor del boton accion
+  //         $conn->close();   
+  // }// Fin del switch, para validar el valor del boton accion
 
 
 ?>
