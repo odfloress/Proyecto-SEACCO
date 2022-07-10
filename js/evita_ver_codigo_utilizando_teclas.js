c8931381e0b72648ae1e1c47ec46dddc93701655
odfloress <odfloress@unah.hn>
2022-07-09 21:52:12 -0600

    Cambios 9 de julio

---
+++ js/evita_ver_codigo_utilizando_teclas.js
@@ -0,0 +1,34 @@
+// inicio evitar si preciona F12, si preciona Ctrl+Shift+I, si preciona Ctr+u 
+
+
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
+return false;
+}
+}
+
+// Fin evitar si preciona F12, si preciona Ctrl+Shift+I, si preciona Ctr+u 
