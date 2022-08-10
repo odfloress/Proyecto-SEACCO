2a95b0e7a14fbb4e6672739b21e3a492991d4bd1
FavielCruz <faviel.cruz@unah.hn>
2022-08-10 16:13:05 -0600

    Cambios 10/8/22 vista clientes

--- vistas/personas/vista_clientes.php
+++ vistas/personas/vista_clientes.php
@@ -278,7 +278,7 @@ if (mysqli_num_rows($roles35) > 0)
                                 <div class="modal-body">
                                 <input type="hidden" name="id_cliente" value="<?php echo $filas['ID_CLIENTE'] ?>">
                 <label for="">Codigo</label>
-                <input type="text" class="form-control" autocomplete="off" name="codigo" required value="<?php echo $filas['CODIGO'] ?>" placeholder="">
+                <input type="text" class="form-control" autocomplete="off" name="codigo" required value="<?php echo $filas['CODIGO'] ?>" placeholder="" onkeyup="mayus(this);">
                 <br>
                 <label for="">Nombre</label>
                 <input type="text" autocomplete="off"  value="<?php echo $filas['NOMBRE_CLIENTE'] ?>" onkeyup="mayus(this);" maxlength="255" class="form-control"  placeholder="" name="nombre" required>
