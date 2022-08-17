fb6c3db7b224501fd60d229b9f8718e480ac6aa7
odfloress <odfloress@unah.hn>
2022-08-16 21:50:22 -0600

    Cambios 16 de agosto

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -7,6 +7,7 @@ $dbname = "seacco_bd";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn->set_charset('utf8');
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
