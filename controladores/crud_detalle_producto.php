b4641837c6f6d0bfe1a92362831393579fb206a3
odfloress <odfloress@unah.hn>
2022-11-24 23:45:41 -0600

    CAMBIOS 24/11/2022 11:45PM

--- controladores/crud_detalle_producto.php
+++ controladores/crud_detalle_producto.php
@@ -29,67 +29,45 @@
   $precio=(isset($_POST['precio']))?$_POST['precio']:"";
   $anterior=(isset($_POST['nombre_anterior']))?$_POST['nombre_anterior']:"";
   
-  
-
- 
-
-
   //variable para recuperar los botones de la vista roles  
   $accion=(isset($_POST['accion']))?$_POST['accion']:"";
   
+  date_default_timezone_set("America/Guatemala");
+  $fechasss = date("Y-m-d h:i:s");
   
   switch($accion){
-  
+  /////////////////////////////////// PARA INSERTAR ////////////////////////////////////
       case "agregar": 
-        // valida si ya esta agregado el producto
-        $validar_producto = "SELECT * FROM tbl_detalle_compra WHERE ID_PRODUCTO='$producto' and ID_COMPRA='$id_compra'";
-        $result1 = mysqli_query($conn, $validar_producto); 
-         if (mysqli_num_rows($result1) > 0) { 
-                
+       ///////////// INSERTA EN LA TABLA TBL_DETALLE COMPRA /////////////
+
+        $sql1 = "INSERT INTO tbl_detalle_compra (ID_COMPRA, ID_PRODUCTO, GARANTIA, ID_UNIDAD_MEDIDA, CANTIDAD, PRECIO)
+        VALUES ('$id_compra', '$producto', '$garantia14', '$unidad_medida14', '$cantidad', '$precio')";
+         if (mysqli_query($conn, $sql1)) 
+         {
+           ///////////// SUMA EL PRECIO TOTAL EN LA TABLA TBL_COMPRA /////////////
+            $total = $cantidad * $precio;
+            $total_compra = "UPDATE tbl_compras SET TOTAL_COMPRA=TOTAL_COMPRA + $total WHERE ID_COMPRA='$id_compra'";
+            if (mysqli_query($conn, $total_compra)){}
+
+            ///////////// INSERTA EN LA TABLA TBL_KARDEX /////////////
+            $kardex = "INSERT INTO tbl_kardex (ID_PRODUCTO, ID_COMPRA, USUARIO, CANTIDAD, TIPO_MOVIMIENTO, FECHA_HORA)
+            VALUES ('$producto', '$id_compra', '$usuario1[usuario]', '$cantidad', 'ENTRADA COMPRA', '$fechasss')";
+            if (mysqli_query($conn, $kardex)) 
+             {
+               ///////////// SUMA AL INVENTARIO /////////////
+               $inventario = "UPDATE tbl_inventario SET CANTIDAD_DISPONIBLE=CANTIDAD_DISPONIBLE + $cantidad WHERE ID_PRODUCTOS='$producto'";
+                if (mysqli_query($conn, $inventario)) {}
+
+             }
                 echo '<script>
-                        alert("Error, el producto ya esta agregardo.");
+                         alert("Producto agregado con exito");
+                         window.location.href="../../vistas/inventario/vista_detalle_producto.php";                   
                       </script>';
-                      mysqli_close($conn);
-         }else{ 
-
-                    ///////////// INSERTA EN LA TABLA TBL_DETALLE COMPRA /////////////
-
-                    $sql1 = "INSERT INTO tbl_detalle_compra (ID_COMPRA, ID_PRODUCTO, GARANTIA, ID_UNIDAD_MEDIDA, CANTIDAD, PRECIO)
-                    VALUES ('$id_compra', '$producto', '$garantia14', '$unidad_medida14', '$cantidad', '$precio')";
-                    if (mysqli_query($conn, $sql1)) {
-
-                   ///////////// SUMA EL PRECIO TOTAL EN LA TABLA TBL_COMPRA /////////////
-
-                   $total = $cantidad * $precio;
-                   $total_compra = "UPDATE tbl_compras SET TOTAL_COMPRA=TOTAL_COMPRA + $total WHERE ID_COMPRA='$id_compra'";
-                   if (mysqli_query($conn, $total_compra)) {}
-
-
-                  ///////////// INSERTA EN LA TABLA TBL_KARDEX /////////////
-                  $kardex = "INSERT INTO tbl_kardex (ID_PRODUCTO, ID_COMPRA, USUARIO, CANTIDAD, TIPO_MOVIMIENTO)
-                  VALUES ('$producto', '$id_compra', '$usuario1[usuario]', '$cantidad', 'ENTRADA')";
-                  if (mysqli_query($conn, $kardex)) 
-                      {
-                        ///////////// SUMA AL INVENTARIO /////////////
-                        $inventario = "UPDATE tbl_inventario SET CANTIDAD_DISPONIBLE=CANTIDAD_DISPONIBLE + $cantidad WHERE ID_PRODUCTOS='$producto'";
-                        if (mysqli_query($conn, $inventario)) {}
-
-                      }
-
-                      echo '<script>
-                                alert("Producto agregado con exito");
-                                window.location.href="../../vistas/inventario/vista_detalle_producto.php";                   
-                            </script>';
-                               
-
-                    } else {
-                            echo '<script>
-                                    alert("Error al tratar de agregar el producto");
-                                  </script>'; 
-                               
-                           }
-                    
-              }                    
+         }else{
+                echo '<script>
+                        alert("Error al tratar de agregar el producto");
+                      </script>';          
+              }                  
       break;
 
       case "cancelar";
@@ -100,75 +78,68 @@
                 ///////////// ELIMINA DE LA TABLA KARDEX /////////////
                 $kardex1 = "DELETE FROM tbl_kardex WHERE ID_COMPRA='$id_compra'";
                 if (mysqli_query($conn, $kardex1)) {}
+
                 ///////////// ELIMINA LA COMPRA DE LA TABLA DE COMPRAS /////////////
                 $compra1 = "DELETE FROM tbl_compras WHERE ID_COMPRA='$id_compra'";
                 if (mysqli_query($conn, $compra1)) {}
-                ///////////// INSERTA EN BITACORA /////////////
-                // inicio inserta en la tabla bitacora
-                $sql = "INSERT INTO tbl_bitacora (USUARIO, ACCION, OBSERVACION)
-                VALUES ('$usuario1[usuario]', 'ELIMINO', 'CANCELO UNA COMPRA')";
-                if (mysqli_query($conn, $sql)) {} else {}
-                // fin inserta en la tabla bitacora
+
+                ///////////// RESTA AL INVENTARIO atraves de un trigger ubicado en la tabla tbl_detalle_compra /////////////
                 echo '<script>
-                                alert("Compra eliminada con exito");
-                                window.location.href="../../vistas/inventario/vista_compras.php";                   
-                            </script>';
+                         alert("Compra eliminada con exito");
+                         window.location.href="../../vistas/inventario/vista_compras.php";                   
+                      </script>';
       }
-      break;
-      case "confirmar";
-      $confirmar = "UPDATE tbl_compras SET ESTADO_COMPRA='FINALIZADO' WHERE ID_COMPRA='$id_compra'";
-      if (mysqli_query($conn, $confirmar)) 
-      {
-        ///////////// INSERTA EN BITACORA /////////////
-                // inicio inserta en la tabla bitacora
-                $sql = "INSERT INTO tbl_bitacora (USUARIO, ACCION, OBSERVACION)
-                VALUES ('$usuario1[usuario]', 'FINALIZO', 'COMPLETO UNA COMPRA')";
-                if (mysqli_query($conn, $sql)) {} else {}
-                // fin inserta en la tabla bitacora
-        echo '<script>
-                alert("Compra completada con exito");
-                window.location.href="../../vistas/inventario/vista_compras.php";                   
-              </script>';
-      }
-              
-      break;
 
       break;
-      //para eliminar en la tabla mysl  
-      case "eliminar";
-    
+
+      case "confirmar";
+      $pre_confirmacion = "SELECT * FROM  tbl_detalle_compra WHERE ID_COMPRA='$id_compra'";
+      $pre_result = mysqli_query($conn, $pre_confirmacion);
       
+      if (mysqli_num_rows($pre_result) > 0) 
+      {
+        $confirmar = "UPDATE tbl_compras SET ESTADO_COMPRA='FINALIZADO' WHERE ID_COMPRA='$id_compra'";
+         if (mysqli_query($conn, $confirmar)) 
+             {
+               echo '<script>
+                        alert("Compra completada con exito");
+                        window.location.href="../../vistas/inventario/vista_compras.php";                   
+                     </script>';
+             }
+       }else{
+              echo '<script>
+                        alert("Debe seleccionar producto para confirmar");
+                        window.location.href="../../vistas/inventario/vista_detalle_producto.php";                   
+                     </script>';
+            }  
+
+      break;
+
      
+      //para eliminar en la tabla mysl  
+      case "eliminar"; 
      
        ///////////// Resta EL PRECIO TOTAL EN LA TABLA TBL_COMPRA /////////////
-
        $total = $cantidad * $precio;
        $total_compras = "UPDATE tbl_compras SET TOTAL_COMPRA=TOTAL_COMPRA - $total WHERE ID_COMPRA=$id_compra";
        if (mysqli_query($conn, $total_compras)) {}
-      
-       ///////////// RESTA AL INVENTARIO /////////////
-//        $inventario = "UPDATE tbl_inventario SET CANTIDAD_DISPONIBLE=CANTIDAD_DISPONIBLE - $cantidad WHERE ID_PRODUCTOS='$producto'";
-//        if (mysqli_query($conn, $inventario)) {}
 
         ///////////// ELIMINA DE LA TABLA KARDEX /////////////
-       $sql3 = "DELETE FROM tbl_kardex WHERE ID_PRODUCTO='$producto' and ID_COMPRA='$id_compra' ";
+        $sql3 = "DELETE FROM tbl_kardex WHERE ID_PRODUCTO='$producto' and ID_COMPRA='$id_compra' ";
         if (mysqli_query($conn, $sql3)) {}
+
         ///////////// ELIMINA DE LA TABLA DETALLE DE COMPRA /////////////
-                        $sql3 = "DELETE FROM tbl_detalle_compra WHERE ID_PRODUCTO='$producto'";
-                        if (mysqli_query($conn, $sql3)) {
-                               
-
-                            header('Location: ../../vistas/inventario/vista_detalle_producto.php');
-                        }else{
-                                echo '<script>
-                                        alert("Error al tratar de eliminar el producto");
-                                    </script>'; mysqli_error($conn);
-                            }
+        $sql3 = "DELETE FROM tbl_detalle_compra WHERE ID_PRODUCTO='$producto'";
+        if (mysqli_query($conn, $sql3)) 
+        {
+            header('Location: ../../vistas/inventario/vista_detalle_producto.php');
+        }else{
+                 echo '<script>
+                          alert("Error al tratar de eliminar el producto");
+                       </script>'; mysqli_error($conn);
+             }
+        ///////////// RESTA AL INVENTARIO atraves de un trigger ubicado en la tabla tbl_detalle_compra /////////////
                         mysqli_close($conn);
-                   
-                        
-          
-
       break;
       
       default:
